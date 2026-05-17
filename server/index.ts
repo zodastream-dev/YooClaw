64dea652c8b9183e622e66b501a1fb15759fcba1
陆峻 <lujun@example.com>
2026-05-17 22:39:00 +0800

    fix: 酷炫模板renderIntelFeed单引号转义bug导致window.open('_blank')JS语法错误

--- server/index.ts
+++ server/index.ts
@@ -4635,7 +4635,7 @@ function renderIntelFeed(data){
   data.forEach(function(item,i){
     var keywords=(item.keywords||[]).slice(0,3);
     var url=item.url||item.link||item.sourceUrl||item.href||'';
-    var clickAttr=url?' data-url="'+escHtml(url)+'" onclick="if(this.dataset.url)window.open(this.dataset.url,\'_blank\')"':'';
+    var clickAttr=url?' data-url="'+escHtml(url)+'" onclick="if(this.dataset.url)window.open(this.dataset.url,\\'_blank\\')"':'';
     html+='<div class="intel-card"'+clickAttr+'>';
     html+='<div class="intel-card-header">';
     if(url){
